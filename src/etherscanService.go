773cc542c79a5d05888ca392297d8455bd33ec7d
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-07-18 17:09:42 +0300

    count balances for 100 blocks, add api key, add retry on max limit error

--- src/etherscanService.go
+++ src/etherscanService.go
@@ -5,10 +5,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func getBlockByTag(tag string) Block {
-	url := fmt.Sprintf("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=%s&boolean=true", tag)
+	if tag[len(tag)-1] == 10 {
+		tag = tag[:len(tag)-1]
+	}
+request:
+	url := fmt.Sprintf("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=%s&boolean=true&apikey=8FMZ3ADCFS5JFX6JBMTY6P71C2I1UMB43T", tag)
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Print(err)
@@ -25,8 +30,10 @@ func getBlockByTag(tag string) Block {
 
 	block := Block{}
 	if _, ok := data["status"]; ok {
-		fmt.Println("tag not valid / too many requests for one tag per 5 minutes\ntag: ", tag)
-		return block
+		fmt.Println(data["result"], "\ntag ", tag, "\ntry again in 1 sec")
+		//todo probably not the best solution, change
+		time.Sleep(1 * time.Second)
+		goto request
 	}
 	s := data["result"]
 	jsonString, _ := json.Marshal(s)
@@ -40,25 +47,13 @@ func getSampleBlock() Block {
 		"\"number\":\"0x10d4f\",\"stateRoot\":\"0xd64a0f63e2c7f541e6e6f8548a10a5c4e49fda7ac1aa80f9dddef648c7b9e25f\"," +
 		"\"timestamp\":\"0x55c9ea07\",\"transactions\":[{\"blockNumber\":\"0x10d4f\"," +
 		"\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-		"\"to\":\"0xbf3403210f9802205f426759947a80a9fda71b1e\",\"value\":\"0xaa9f075c200000\"}," +
+		"\"to\":\"0xbf3403210f9802205f426759947a80a9fda71b1e\",\"value\":\"0x1\"}," +
 		"{\"blockNumber\":\"0x10d4f\",\"from\":\"0xd2d5862c001b7ba77970e13c173356bf1a551e2e\"," +
-		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0xe82f47fbea6c200\"}," +
+		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x2\"}," +
 		"{\"blockNumber\":\"0x10d4f\",\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0xde4289155b36ecc\"}," +
+		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x3\"}," +
 		"{\"blockNumber\":\"0x10d4f\",\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x0\"}]}"
-
-	//s := "{\"difficulty\":\"0x1d95715bd14\",\"hash\":\"0x7eb7c23a5ac2f2d70aa1ba4e5c56d89de5ac993590e5f6e79c394e290d998ba8\"," +
-	//	"\"number\":\"0x10d4f\",\"stateRoot\":\"0xd64a0f63e2c7f541e6e6f8548a10a5c4e49fda7ac1aa80f9dddef648c7b9e25f\"," +
-	//	"\"timestamp\":\"0x55c9ea07\",\"transactions\":[{\"blockNumber\":\"0x10d4f\"," +
-	//	"\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-	//	"\"to\":\"0xbf3403210f9802205f426759947a80a9fda71b1e\",\"value\":\"0x1\"}," +
-	//	"{\"blockNumber\":\"0x10d4f\",\"from\":\"0xd2d5862c001b7ba77970e13c173356bf1a551e2e\"," +
-	//	"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x2\"}," +
-	//	"{\"blockNumber\":\"0x10d4f\",\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-	//	"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x3\"}," +
-	//	"{\"blockNumber\":\"0x10d4f\",\"from\":\"0x4458f86353b4740fe9e09071c23a7437640063c9\"," +
-	//	"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x4\"}]}"
+		"\"to\":\"0x00000000b7ca7e12dcc72290d1fe47b2ef14c607\",\"value\":\"0x4\"}]}"
 	var block Block
 	json.Unmarshal([]byte(s), &block)
 
@@ -66,7 +61,8 @@ func getSampleBlock() Block {
 }
 
 func getLastBlockNumber() string {
-	resp, err := http.Get("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber")
+request:
+	resp, err := http.Get("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=8FMZ3ADCFS5JFX6JBMTY6P71C2I1UMB43T")
 	if err != nil {
 		panic(err)
 	}
@@ -77,8 +73,10 @@ func getLastBlockNumber() string {
 	err = json.Unmarshal([]byte(body), &data)
 
 	if _, ok := data["status"]; ok {
-		fmt.Println("Max rate limit reached (requests to 1 url per 3-5 minutes)")
-		return ""
+		fmt.Println("Max rate limit reached")
+		//todo probably not the best solution, change
+		time.Sleep(1 * time.Second)
+		goto request
 	}
 
 	return data["result"]
--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/holiman/uint256"
 	"hash/fnv"
+	"strconv"
+	"time"
 )
 
 // Напиши программу, которая выдаст адрес,
@@ -12,9 +14,6 @@ import (
 
 // что за адрес нужно отслеживать - адрес отправителя, получателя? оба? повторяются ли транзакции для отправителя/получателя?
 
-// map адресов, каждый раз, когда встречается адрес, надо значение изменять на число value
-// map[адрес][сумма]
-
 // изменение по абсолютной величине - это просто сумма всех именений без учета знака?
 // т.е. здесь: +4, -3, -2 абсолютное изменение это 9 или -1?
 
@@ -22,68 +21,69 @@ var addresses = make(map[uint32]string)
 var balances = make(map[uint32]*uint256.Int)
 
 func main(){
+	start := time.Now()
 
-	last := getBlockByTag("0xc3981b")
-	//last := getSampleBlock()
-
-	for _, trans := range last.Transactions {
-		bufValue := uint256.NewInt(0)
-		curFromAddr := hash(trans.From)
-		curToAddr := hash(trans.To)
-		curValue, err := uint256.FromHex(trans.Value)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("from: ", curFromAddr, " to: ", curToAddr,  " value: ", curValue)
-
-		if curValue.IsZero() {
-			continue
-		}
-
-		// should addresses be added before or after zero check?
-		if _, ok := addresses[curFromAddr]; !ok {
-			addresses[curFromAddr] = trans.From
-		}
-		if _, ok := addresses[curToAddr]; !ok {
-			addresses[curToAddr] = trans.To
-		}
-
-		if _, ok := balances[curFromAddr]; ok {
-			bufValue.Add(balances[curFromAddr], curValue)
-			balances[curFromAddr] = bufValue
-		} else {
-			//curValue.Neg(curValue)
-			balances[curFromAddr] = curValue
-		}
-
-		if _, ok := balances[curToAddr]; ok {
-			bufValue.Add(balances[curToAddr], curValue)
-			balances[curToAddr] = bufValue
-		} else {
-			balances[curToAddr] = curValue
-		}
+	lastBlockNumber := getLastBlockNumber()
+	last, err := strconv.ParseInt(lastBlockNumber, 0, 64)
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println(addresses)
-	fmt.Println()
-	for i,j := range balances {
-		fmt.Println(i, ":  ", j)
+	for i := last - 99; i <= last; i++ {
+		curTag := fmt.Sprintf("0x%x\n", i)
+
+		//need to sleep here or smth 'cause max rate limit hits "5 calls per sec/IP" (c) docs <- for registered accounts, with API key
+		//time.Sleep(5 * time.Second)
+		curBlock := getBlockByTag(curTag)
+
+		for _, transaction := range curBlock.Transactions {
+			bufValue := uint256.NewInt(0)
+			curFromAddr := hash(transaction.From)
+			curToAddr := hash(transaction.To)
+			curValue, err := uint256.FromHex(transaction.Value)
+			if err != nil {
+				panic(err)
+			}
+
+			//fmt.Println("from: ", curFromAddr, " to: ", curToAddr,  " value: ", curValue)
+
+			if curValue.IsZero() {
+				continue
+			}
+
+			// should addresses be added before or after zero check?
+			if _, ok := addresses[curFromAddr]; !ok {
+				addresses[curFromAddr] = transaction.From
+			}
+			if _, ok := addresses[curToAddr]; !ok {
+				addresses[curToAddr] = transaction.To
+			}
+
+			if _, ok := balances[curFromAddr]; ok {
+				bufValue.Add(balances[curFromAddr], curValue)
+				balances[curFromAddr] = bufValue
+			} else {
+				//curValue.Neg(curValue)
+				balances[curFromAddr] = curValue
+			}
+
+			if _, ok := balances[curToAddr]; ok {
+				bufValue.Add(balances[curToAddr], curValue)
+				balances[curToAddr] = bufValue
+			} else {
+				balances[curToAddr] = curValue
+			}
+		}
+
 	}
 
-///////////////////////////////
+	fmt.Println(len(balances))
+	//for m,n := range balances {
+	//	fmt.Println(m, ":  ", n)
+	//}
 
-	//lastBlockNumber, _ := strconv.ParseInt(last, 0, 64)
-	//fmt.Println(lastBlockNumber)
-
-	//balances := make(map[string]int)
-
-	//addr, ok := balances[address]
-
-	//for i := lastBlockNumber - 99; i <= lastBlockNumber; i++ {
-		//get each block and count whatever is needed to be counted
-		//getBlock() // i to hex
-
+	elapsed := time.Since(start)
+	fmt.Printf("Time %s", elapsed)
 }
 
 
