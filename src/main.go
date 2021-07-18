19aadf5d03e617558f0c021bb6bcbdf74bb353ef
mishkamashka <anisimova.mariia.mm@gmail.com>
2021-07-18 14:22:43 +0300

    impl balance counting for one block

--- src/main.go
+++ src/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"hash/fnv"
-
 	"github.com/holiman/uint256"
+	"hash/fnv"
 )
 
 // Напиши программу, которая выдаст адрес,
@@ -15,68 +14,63 @@ import (
 // map адресов, каждый раз, когда встречается адрес, надо значение изменять на число value
 // map[адрес][сумма]
 
-// что-то делать с value uint256...
-// может стоит сразу отметать нули
+// изменение по абсолютной величине - это просто сумма всех именений без учета знака?
+// т.е. здесь: +4, -3, -2 абсолютное изменение это 9 или -1?
 
 var addresses = make(map[uint32]string)
 
 func main(){
-	//last := getSampleBlock()
 
-	//balances := make(map[uint64]*uint256.Int)
+	last := getSampleBlock()
 
-
-	var a,b,c,d *uint256.Int
-	a, _ = uint256.FromHex("0xfffffffffffffffffffffffffffffffffffffffffffffffc8fbb078f9332b04a")
-	b, _ = uint256.FromHex("0xde4289155b36ecc")
-	c, _ = uint256.FromHex("0xaa9f075c200000")
-	d, _ = uint256.FromHex("0xaa9f075c200000")
-
-	c.Add(a,d.Neg(b))
-
-	fmt.Print(a, " + ", b, " = ", c)
-
-	//for _, trans := range last.Transactions {
-	//
-	//	curFromAddr := hash(trans.From)
-	//	curToAddr := hash(trans.To)
-	//
-	//	if _, ok := addresses[curFromAddr]; !ok {
-	//		addresses[curFromAddr] = trans.From
-	//	}
-	//	if _, ok := addresses[curToAddr]; !ok {
-	//		addresses[curToAddr] = trans.To
-	//	}
-	//
-	//	transValue, _ := strconv.ParseUint(trans.Value, 0, 64)
-	//	fmt.Println(transValue)
-	//
-	//	if transValue == 0 {
-	//		break
-	//	}
-
-		//if value, ok := balances[curFromAddr]; ok {
-		//	fmt.Println(value)
-		//
-		//	balances[curFromAddr] = value - trans.Value
-		//} else {
-		//	fmt.Println("no")
-		//	balances[curFromAddr] = - trans.Value
-		//}
-		//
-		//if value, ok := balances[curToAddr]; ok {
-		//	fmt.Println(value)
-		//	balances[curToAddr] = value + trans.Value
-		//} else {
-		//	fmt.Println("no")
-		//	balances[curToAddr] = trans.Value
-		//}
-	//}
+	balances := make(map[uint32]*uint256.Int)
 
 
+	for _, trans := range last.Transactions {
 
+		var bufValue *uint256.Int
+		bufValue, _ = uint256.FromHex("0x0")
 
+		curFromAddr := hash(trans.From)
+		curToAddr := hash(trans.To)
+		curValue, err := uint256.FromHex(trans.Value)
+		if err != nil {
+			panic(err)
+		}
 
+		if curValue.IsZero() {
+			break
+		}
+
+		// should addresses be added before or after zero check?
+		if _, ok := addresses[curFromAddr]; !ok {
+			addresses[curFromAddr] = trans.From
+		}
+		if _, ok := addresses[curToAddr]; !ok {
+			addresses[curToAddr] = trans.To
+		}
+
+		if _, ok := balances[curFromAddr]; ok {
+			bufValue.Add(balances[curFromAddr], curValue)
+			balances[curFromAddr] = bufValue
+		} else {
+			//curValue.Neg(curValue)
+			balances[curFromAddr] = curValue
+		}
+
+		if _, ok := balances[curToAddr]; ok {
+			bufValue.Add(balances[curToAddr], curValue)
+			balances[curToAddr] = bufValue
+		} else {
+			balances[curToAddr] = curValue
+		}
+	}
+
+	fmt.Println(addresses)
+	fmt.Println()
+	for i,j := range balances {
+		fmt.Println(i, ":  ", j)
+	}
 
 
 	//lastBlockNumber, _ := strconv.ParseInt(last, 0, 64)
@@ -90,9 +84,8 @@ func main(){
 		//get each block and count whatever is needed to be counted
 		//getBlock() // i to hex
 
-	//}
-
 }
+
 
 func hash(s string) uint32 {
 	h := fnv.New32a()
