a68e0f19e40d0044121f991efab5588ad58ba0ba
Mariia <36487922+mishkamashka@users.noreply.github.com>
2021-07-19 19:51:05 +0300

    Update transaction.go

--- src/transaction.go
+++ src/transaction.go
@@ -2,19 +2,6 @@ package main
 
 type Transaction struct {
 	From		string	`json:"from"`
-	To			string	`json:"to"`
+	To		string	`json:"to"`
 	Value 		string	`json:"value"`
-	BlockNumber	string 	`json:"block_number"`
-
-	//blockHash string
-	//gas string
-	//gasPrice string
-	//hash string
-	//input string
-	//nonce string
-	//r string
-	//s string
-	//transactionIndex string
-	//transactionType string
-	//v string
 }
